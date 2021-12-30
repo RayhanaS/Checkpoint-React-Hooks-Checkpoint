ca2c975411edfa72acc6231c1a152ae8e3351f33
RayhanaS <94605484+RayhanaS@users.noreply.github.com>
2021-12-30 10:07:39 +0100

    first commit

---
+++ src/Component/MovieList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MovieCard from './MovieCard';
+
+function MovieList({movieList}) {
+    return (
+        <div>
+           <div className="movieList">
+              {
+                  movieList.map((el,key) => <MovieCard key={el.id} movie={el}/>)
+              }
+           </div>
+           
+        </div>
+    )
+}
+
+export default MovieList
